6885c3aa3749a3053ba5e4f36002d58176f866d6
HudsonV <vieirahwb@gmail.com>
2019-01-12 16:34:48 +0100

    oi

--- Logipix.java
+++ Logipix.java
@@ -3,7 +3,7 @@ import java.io.File;import java.io.File;
 import java.io.FileWriter;
 import java.io.FileNotFoundException; 
 import java.io.IOException;
-
+ //Oi
 public class Logipix{
 
 	File input;
