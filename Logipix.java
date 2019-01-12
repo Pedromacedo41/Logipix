4aed8f64231c180c4497e12656767432fd29bf79
Pedro Macedo <pedromacedo41@gmail.com>
2019-01-12 14:08:11 +0100

    Print functions

--- Logipix.java
+++ Logipix.java
@@ -1,5 +1,6 @@
 import java.util.*;
-import java.io.File;
+import java.io.File;import java.io.File;
+import java.io.FileWriter;
 import java.io.FileNotFoundException; 
 import java.io.IOException;
 
@@ -13,7 +14,7 @@ public class Logipix{
 	public static void main(String[] args) throws FileNotFoundException {
 		Logipix logipix = new Logipix();
 		logipix.initialize("InputFiles/LogiX.txt");
-		logipix.print();
+		logipix.example();
 	}
 
 	public void initialize(String name){
@@ -63,13 +64,54 @@ public class Logipix{
 		   	}
 		   	System.out.print("\n");
 		 }
+		 printBrokenLines();
+    }
+
+    private void printBrokenLines(){
+    	for (int i=0; i < brokenLines.size(); i++ ) {
+    		LinkedList<Cell> line = brokenLines.get(i);
+    	}
+    }
+
+    private void printLine(LinkedList<Cell> line){
+    	Position pos; 
+    	Cell[] Line = (Cell[]) line.toArray();
+    	for (int i=0; i < (Line.length-1) ; i++) {    		
+    		if(Line[i].x != Line[i+1].x){
+    			if(Line[i].x > Line[i+1].x) pos= Position.LEFT;
+    			else pos = Position.RIGHT;
+    		}else{
+    			if(Line[i].y > Line[i+1].y) pos= Position.UP;
+    			else pos = Position.DOWN;
+    		}
+    	}
+
+
+
+
     }
 
     public void example(){
+    	LinkedList<Cell> a = new LinkedList<>();
+    	a.add(GameGrid[0][0]);
+    	a.add(GameGrid[0][1]);
+    	brokenLines.add(a);
 
-    }
+    	LinkedList<Cell> b = new LinkedList<>();
+    	b.add(GameGrid[8][0]);
+    	b.add(GameGrid[8][1]);
+    	b.add(GameGrid[8][2]);
+    	b.add(GameGrid[7][2]);
+    	brokenLines.add(b);
 
-    private void modify(Cell voisin1, Cell voisin2){
-
+    	LinkedList<Cell> c = new LinkedList<>();
+    	c.add(GameGrid[5][4]);
+    	c.add(GameGrid[5][5]);
+    	c.add(GameGrid[6][5]);
+    	c.add(GameGrid[6][4]);
+    	c.add(GameGrid[6][3]);
+    	c.add(GameGrid[7][3]);
+    	brokenLines.add(c);
+    	print();
     }
 }
